194f1975f368002dccc45fcb1435ff69d94eeee6
lukasisaksson <116803275+lukasisaksson@users.noreply.github.com>
2023-02-09 11:38:42 +0100

    Rättningar

--- js/script.js
+++ js/script.js
@@ -33,22 +33,3 @@ window.onclick = function(event) {
   }
 }
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction2() {
-  document.getElementById("subdropdown").classList.toggle("show2");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn2')) {
-    var dropdowns = document.getElementsByClassName("dropdown2-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show2')) {
-        openDropdown.classList.remove('show2');
-      }
-    }
-  }
-}
